73d4972d5f7490d9bae87f1504fac33683985d7c
tonyfast <tony.fast@gmail.com>
2017-05-19 23:49:45 -0400

    Add extension

--- fidget/__init__.py
+++ fidget/__init__.py
@@ -22,3 +22,24 @@ del func, name
 __all__ += ['call']
 
 Calls.namespaces['toolz'] = vars(__import__('toolz'))
+
+try:
+    from IPython import get_ipython
+
+    class Magic(Calls):
+        def _decorate_(self, function):
+            return get_ipython().register_magic_function(
+                calls[does[stars.second()[function.function]]][None],
+                magic_kind='cell',
+                magic_name=self.args[0])
+
+    __all__.append('Magic')
+except:
+    pass
+
+
+def load_ipython_extension(ip):
+    """%%fidget magic that displays code cells as markdown, then runs the cell.
+    """
+    from IPython import display
+    Magic('fidget')[does[display.Markdown][display.display], ip.run_cell]()
