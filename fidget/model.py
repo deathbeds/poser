d163569f629810e4ac4c3c9a4e5410f481eff21a
tonyfast <tony.fast@gmail.com>
2017-05-21 10:11:42 -0400

    Objects.namespaces -> model

--- fidget/model.py
+++ fidget/model.py
@@ -1,36 +1,53 @@
 # coding: utf-8
 
+
 try:
-    from .objects import Functions, Compose, Calls, Composer
+    from .objects import  Functions, Compose, Calls, Composer
     from .callables import flipped, do, step, starred, excepts, ifnot, ifthen
 except Exception as e:
-    from objects import Functions, Compose, Calls, Composer
+    from objects import  Functions, Compose, Calls, Composer
     from callables import flipped, do, step, starred, excepts, ifnot, ifthen
-
-from functools import wraps
+    
+from collections import OrderedDict
+from functools import partial, wraps
+from six import PY3
 from operator import attrgetter
-from toolz.curried import (isiterable, flip, complement, interpose, groupby,
-                           reduce, filter, map)
+from toolz.curried import (isiterable, flip, complement, interpose, groupby, merge, reduce, filter, map)
 _attribute_ = "__{}{}__".format
 
 __all__ = ['flips', 'stars', 'does', 'maps', 'filters', 'groups', 'reduces']
 functions = (flipped, starred, do, map, filter, groupby, reduce)
 
+class Namespaces(Calls):
+    namespaces = OrderedDict({'fidget': {}})
+    def __getattr__(self, attr):
+        for namespace in self.namespaces.values():
+            if attr in namespace:
+                callable = namespace[attr]
+                doc = callable.__doc__
+                if callable in type(self).__dict__.values():
+                    callable = partial(callable, self)
+                else:
+                    callable = partial(self.__getitem__, callable)
+                return PY3 and setattr(callable, '__doc__',  doc) or callable
+        raise AttributeError("No attribute {}".format(attr))
+        
+    def __dir__(self):
+        return list(super(Namespaces, self).__dir__()) + list(merge(self.namespaces.values()).keys())
 
-class Models(Calls):
+class Models(Namespaces):    
     def __xor__(self, object):
         self, _isinstance = self[:], flip(isinstance)  # noqa: F823
         if not isiterable(object) and isinstance(object, type):
-            object = (object, )
+            object = (object,)
         if isiterable(object):
-            if all(map(_isinstance(type), object)) and all(
-                    map(flip(issubclass)(BaseException), object)):
+            if all(map(_isinstance(type), object)) and all(map(flip(issubclass)(BaseException), object)):
                 self.function = Compose(excepts(object, self.function))
                 return self
-
+            
             if all(map(_isinstance(BaseException), object)):
                 object = tuple(map(type, object))
-
+                
             if all(map(_isinstance(type), object)):
                 object = _isinstance(object)
 
@@ -41,60 +58,47 @@ class Models(Calls):
         self = self[:]
         self.function = Compose([ifnot(self.function, Compose([object]))])
         return self
-
+    
     def __and__(self, object):
         self = self[:]
         self.function = Compose([step(self.function, Compose([object]))])
         return self
-
+    
     def __pos__(self):
         return self[bool]
 
     def __neg__(self):
         return self[complement(bool)]
-
+        
     def __lshift__(self, object):
         return Does()[object] if self._factory_ else self[do(object)]
-
+    
     def __round__(self, n):
         self.function.function = list(interpose(n, self.function.function))
         return self
-
+    
     __invert__, __pow__ = Functions.__reversed__, __xor__
     __mul__ = __add__ = __rshift__ = __sub__ = Composer.__getitem__
 
-
 for name, function in zip(__all__, functions):
-    locals().update({
-        name.capitalize():
-        type(name, (Models, ), {'_decorate_': staticmethod(function)})
-    })
+    locals().update({name.capitalize(): type(name, (Models,), {'_decorate_': staticmethod(function)})})
 
 __all__ += ['models']
 
 for fidget in __all__:
     callable = locals()[fidget.capitalize()]
-    locals()[fidget] = type('_{}_'.format(fidget.capitalize()), (callable, ),
-                            {})(function=Compose([callable]))
+    locals()[fidget] = type('_{}_'.format(fidget.capitalize()), (callable,), {})(function=Compose([callable]))
 
-for op, func in (('matmul', 'groupby'), ('truediv', 'map'),
-                 ('floordiv', 'filter'), ('mod', 'reduce')):
+for op, func in (('matmul', 'groupby'), ('truediv', 'map'), ('floordiv', 'filter'), ('mod', 'reduce')):
     setattr(Models, _attribute_('', op), property(Compose(attrgetter(func))))
-Models.__div__ = Models.__truediv__
-
+Models.__div__  = Models.__truediv__
 
 def fallback(attr):
     def fallback(right, left):
         right = right[:]
         return getattr(type(right)()[left], attr)(right)
-
     return wraps(getattr(Models, attr))(fallback)
 
-
-for attr in [
-        'add', 'sub', 'mul', 'matmul', 'div', 'truediv', 'floordiv', 'mod',
-        'lshift', 'rshift', 'and', 'xor', 'or', 'pow'
-]:
-    setattr(Models,
-            _attribute_('i', attr), getattr(Models, _attribute_('', attr)))
+for attr in ['add', 'sub', 'mul', 'matmul','div', 'truediv', 'floordiv', 'mod', 'lshift', 'rshift', 'and', 'xor', 'or', 'pow']:
+    setattr(Models, _attribute_('i', attr), getattr(Models, _attribute_('', attr)))
     setattr(Models, _attribute_('r', attr), fallback(_attribute_('', attr)))
--- fidget/objects.py
+++ fidget/objects.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 
+
 try:
     from .state import State
     from .callables import call, functor
@@ -9,72 +10,50 @@ except:
     from callables import call, functor
 
 from copy import copy
-from collections import OrderedDict
-from functools import partial
-from toolz.curried import compose, first, isiterable, merge
-from six import iteritems, PY3
-
+from toolz.curried import compose, first, isiterable
+from six import iteritems
 
 class Append(State):
-    __slots__ = ('function', )
-    namespaces = OrderedDict({'fidget': {}})
-
-    def __init__(self, function=None, *args):
+    __slots__ = ('function',)
+    def __init__(self, function=None, *args):      
         if function is None:
             function = list()
-
+        
         if not isiterable(function) or isinstance(function, (str, )):
             function = [function]
-
+            
         super(Append, self).__init__(function, *args)
-
-    def __getitem__(self, object=slice(None)):
+   
+    def __getitem__(self, object=slice(None)):   
         if isinstance(object, Append) and object._factory_:
             object = object()
-
-        if object is call:
+            
+        if object is call: 
             return abs(self)
-
+        
         if isinstance(object, call):
             return object(self)()
-
-        return object != slice(None) and self.append(object) or self
-
+        
+        return object != slice(None) and self.append(object) or self 
+    
     def __repr__(self):
         return repr(self.function)
-
+    
     def append(self, object):
         self.function.append(object)
-
-    def __getattr__(self, attr):
-        for namespace in self.namespaces.values():
-            if attr in namespace:
-                callable = namespace[attr]
-                doc = callable.__doc__
-                if callable in type(self).__dict__.values():
-                    callable = partial(callable, self)
-                else:
-                    callable = partial(self.__getitem__, callable)
-                return PY3 and setattr(callable, '__doc__', doc) or callable
-        raise AttributeError("No attribute {}".format(attr))
-
-    def __dir__(self):
-        return list(super(Append, self).__dir__()) + list(
-            merge(self.namespaces.values()).keys())
-
 
 class Functions(Append):
     def __contains__(self, object):
         return any(object == function for function in self)
-
+    
     def __delitem__(self, object):
         self.function = list(fn for fn in self if fn != object)
         return self
-
+    
     def __setitem__(self, attr, object):
         self.function = list(object if fn == attr else fn for fn in self)
-        return self
-
+        return self  
+    
     def __iter__(self):
         for function in self.function:
             yield function
@@ -83,44 +62,33 @@ class Functions(Append):
         self.function = type(self.function)(reversed(self.function))
         return self
 
-
 class Composite(Functions):
     @staticmethod
     def _dispatch_(function):
-        return isinstance(function, (dict, set, list, tuple)) and Juxtapose(
-            function, type(function)) or functor(function)
-
+        return isinstance(function, (dict, set, list, tuple)) and Juxtapose(function, type(function)) or functor(function)
 
 class Juxtapose(Composite):
     __slots__ = ('function', 'type')
-
-    def __init__(self, function, type_=None):
+    def __init__(self, function, type_=None):      
         if isinstance(function, dict):
             type_ = type(function)
             function = compose(tuple, iteritems)(function)
         super(Juxtapose, self).__init__(function, type_)
 
     def __call__(self, *args, **kwargs):
-        return self.type(
-            call(*args)(self._dispatch_(function))(**kwargs)
-            for function in self)
-
+        return self.type(call(*args)(self._dispatch_(function))(**kwargs) for function in self)
 
 class Compose(Composite):
     def __call__(self, *args, **kwargs):
         for function in self:
-            args, kwargs = (call(*args)(self._dispatch_(function))(
-                **kwargs), ), {}
+            args, kwargs = (call(*args)(self._dispatch_(function))(**kwargs),), {}
         return first(args)
-
 
 class Partial(Functions):
     __slots__ = ('args', 'keywords', 'function')
-
     @property
     def __call__(self):
         return call(*self.args, **self.keywords)
-
 
 class Composer(Partial):
     def __init__(self, *args, **kwargs):
@@ -131,9 +99,8 @@ class Composer(Partial):
 
     @property
     def _factory_(self):
-        return type(self).__name__.startswith('_') and type(
-            self).__name__.endswith('_')
-
+          return type(self).__name__.startswith('_') and type(self).__name__.endswith('_')
+    
     def __getitem__(self, object=slice(None), *args, **kwargs):
         if self._factory_:
             self = self.function()
@@ -142,29 +109,26 @@ class Composer(Partial):
             object, self = self.function.function[object], copy(self)
             self.function = Compose(object)
             return self
-
+               
         return super(Composer, self).__getitem__(
             (args or kwargs) and call(*args, **kwargs)(object) or object)
-
+    
     def append(self, object):
         self.function.function.append(object)
 
-
 class Calls(Composer):
     _decorate_ = staticmethod(functor)
-
+    
     @property
     def __call__(self):
         return super(Calls, self).__call__(self._decorate_(self.function))
 
-
 @property
 def doc(self):
-    string = ""
+    string=""
     for function in self:
         string += getattr(function, 'func', function).__doc__ or "" + "\n---\n"
     return string
-
 
 for klass in (Calls, Partial, Compose, Juxtapose):
     klass.__doc__ = doc
