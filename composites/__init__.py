061fe0b185ffb241ef76093ddb2735af9c8d3396
tonyfast <tony.fast@gmail.com>
2017-12-04 00:28:19 -0500

    Add canonicall composition

--- composites/__init__.py
+++ composites/__init__.py
@@ -2,7 +2,11 @@ from .composites import *
 from .conditions import *
 from .objects import *
 from .attributes import *
+from .canonical import *
+from .partials import *
 
-from . import objects, composites, conditions, attributes
+
+from . import objects, composites, conditions, attributes, partials
 __all__ = objects.__all__ + composites.__all__ + \
-    conditions.__all__ + attributes.__all__
+    conditions.__all__ + attributes.__all__ + partials.__all__  \
+    + ('canonical', 'x')
