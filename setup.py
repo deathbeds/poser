be66176f9d07866097097322909155d5adccfc86
Tony Fast <tony.fast@gmail.com>
2020-04-04 21:41:57 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,43 +1,20 @@
-from pathlib import Path
-import setuptools
+import toml, setuptools, pathlib
 
-name = "poser"
+flit = toml.load('pyproject.toml')['tool']['flit']
+metadata = flit['metadata']
 
-__version__ = "0.2.0"
+module = __import__(metadata['module'])
 
-here = Path(__file__).parent
-
-setup_args = dict(
-    name=name,
-    version=__version__,
-    author="deathbeds",
-    author_email="tony.fast@gmail.com",
-    description="Dysfunctional programming in Python with all the side effects.",
-    long_description=(here / "readme.md").read_text(),
-    long_description_content_type='text/markdown',
-    url="https://github.com/deathbeds/importnb",
-    python_requires=">=3.6",
-    license="BSD-3-Clause",
-    install_requires=[
-        "dataclasses",
-        "toolz",
-        "requests",
-        "joblib"
-    ],
-    include_package_data=True,
-    py_modules=["poser"],
-    classifiers=(
-        "Development Status :: 4 - Beta",
-        "Framework :: IPython",
-        "Framework :: Jupyter",
-        "Intended Audience :: Developers",
-        "Natural Language :: English",
-        "License :: OSI Approved :: BSD License",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3.6",
-    ),
-    zip_safe=False,
+setuptools.setup(
+    name=metadata['module'],
+    version=module.__version__,
+    packages=[metadata['module']],
+    classifiers=metadata['classifiers'],
+    url=metadata['home-page'],
+    author=metadata['author'],
+    author_email=metadata['author-email'],
+    description=module.__doc__,
+    long_description=pathlib.Path(metadata['description-file']).read_text(),
+    long_description_content_type="text/markdown",
+    install_requires=metadata['requires']
 )
-
-if __name__ == "__main__":
-    setuptools.setup(**setup_args)
