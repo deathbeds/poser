886e8032a6d5b506c410ddd0c736d881d0c9ef2d
Tony Fast <tony.fast@gmail.com>
2020-04-06 22:55:53 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ description, version = flit.common.get_docstring_and_version_via_import(flit.com
 
 setuptools.setup(
     name=metadata['module'],
-    version=module.__version__,
+    version=version,
     packages=setuptools.find_packages(),
     classifiers=metadata['classifiers'],
     url=metadata['home-page'],
     author=metadata['author'],
     install_requires=metadata.get('requires', []),
     author_email=metadata['author-email'],
-    description=module.__doc__,
+    description=description,
     long_description=pathlib.Path(metadata['description-file']).read_text(),
     long_description_content_type="text/markdown",
     extras_require=metadata.get('requires-extra', {}),
