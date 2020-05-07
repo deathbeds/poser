959cd903bcb6a83fa91555a6a6ef396ecdfad9df
Tony Fast <tony.fast@gmail.com>
2020-05-07 11:25:01 -0400

    Update __init__.py

--- poser/__init__.py
+++ poser/__init__.py
@@ -1,6 +1,7 @@
 "Symbollic, extensible function composition in python."
 
-from .poser import *
-from .methodz import *
 from . import poser, methodz
 __all__ = poser.__all__ + methodz.__all__
+
+from .poser import *
+from .methodz import *
